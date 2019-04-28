b24ae7bcc2f058d47935c47f3073063275b69d10
David256 <davidborja456@gmail.com>
2019-04-28 14:37:09 -0500

    Sabremos donde todo falla con el log

--- blueLobster.py
+++ blueLobster.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
+import logging
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -10,8 +10,23 @@ from gi.repository import Gtk
 
 from classes.gui import GUI
 
-gui = GUI("Blue Lobster Recorder")
+file = open("/tmp/BlueLobsterRecorder.log", "w")
+
+logger = logging.getLogger("BlueLobsterRecorder")
+formatter = logging.Formatter("%(asctime)s (%(filename)s:%(lineno)d %(threadName)s) %(levelname)s - %(name)s: \"%(message)s\"")
+
+console_output_handler = logging.StreamHandler(file)
+console_output_handler.setFormatter(formatter)
+logger.addHandler(console_output_handler)
+
+logger.setLevel(logging.DEBUG)
+
+logger.debug("Comenzamos")
+
+gui = GUI("Blue Lobster Recorder", logger)
 gui.connect("destroy", Gtk.main_quit)
 gui.show_all()
 
 Gtk.main()
+
+file.close()
